e8a76436adb4b395b4d7b9086517bb3f94a94f6e
Ana Sakotic <sakoticana@gmail.com>
2024-10-20 18:10:41 +0200

    text similarity page

--- src/app/components/text-similarity/text-similarity.component.ts
+++ src/app/components/text-similarity/text-similarity.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Service } from 'src/app/services/entity-extraction.service';
+import { environment } from 'src/app/environments/environment';
 
 @Component({
   selector: 'app-text-similarity',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./text-similarity.component.css']
 })
 export class TextSimilarityComponent {
+  text1: string = ''; 
+  text2: string = '';  
+  result: number | null = null; 
 
+  constructor(private service: Service) { }
+
+  check() {
+    const token = localStorage.getItem('token');  
+    const apiUrl = environment.apiSimilarity;
+
+    const params: any = {
+      text1: this.text1,
+      text2: this.text2,
+      token: token
+    };
+
+    this.service.getResults(apiUrl, params ).subscribe((result: any) => {
+      this.result = result.similarity;
+    });
+  }
 }
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiExtraction: `https://api.dandelion.eu/datatxt/nex/v1/`,
-    apiDetection: `https://api.dandelion.eu/datatxt/li/v1/`
+    apiDetection: `https://api.dandelion.eu/datatxt/li/v1/`,
+    apiSimilarity: `https://api.dandelion.eu/datatxt/sim/v1`,
 }
